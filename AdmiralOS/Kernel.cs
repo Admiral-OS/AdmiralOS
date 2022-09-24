e80572cb8ce6e4c8636fca75550a1fee67749343
BritishGeekGuy <charliedavidwelland@hotmail.com>
2022-09-24 20:27:53 +0100

    Basic Stuff for Alpha 0.1 - Barely anything besides initialising the Hard Drive, an About command, and a useless help command. Nothing useful yet lmao

--- AdmiralOS/Kernel.cs
+++ AdmiralOS/Kernel.cs
@@ -2,24 +2,76 @@
 using System.Collections.Generic;
 using System.Text;
 using Sys = Cosmos.System;
+using Cosmos.System.FileSystem.VFS;
+using Cosmos.System.FileSystem;
+using System.Threading;
 
 namespace AdmiralOS
 {
     public class Kernel : Sys.Kernel
     {
+        CosmosVFS fs = new Sys.FileSystem.CosmosVFS();
 
         protected override void BeforeRun()
         {
             Console.Clear();
-            Console.WriteLine("Cosmos booted successfully. Type a line of text to get it echoed back.");
+            Console.WriteLine("Would you like to Format the Hard Drive in this system to FAT32?");
+            Console.WriteLine("This is required for all Filesystem commands to work");
+            Console.Write("Y/N? ");
+
+            string initinput;
+            string initinLow;
+            initinput = Console.ReadLine();
+            initinLow = initinput.ToLower();
+
+            if (initinLow == "y")
+            {
+                try
+                {
+                    Sys.FileSystem.VFS.VFSManager.RegisterVFS(fs);
+                    Thread.Sleep(3000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                    Thread.Sleep(3000);
+                }
+                
+            }
+            else if (initinLow == "n")
+            {
+                Console.WriteLine("Skipping...");
+                Thread.Sleep(3000);
+            }
+            
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("AdmiralOS - Alpha V0.1");
+            Console.Beep();
         }
 
         protected override void Run()
         {
-            Console.Write("Input: ");
-            var input = Console.ReadLine();
-            Console.Write("Text typed: ");
-            Console.WriteLine(input);
+            string input;
+            string inLow;
+            Console.Write("0:\\");
+            input = Console.ReadLine();
+            inLow = input.ToLower();
+
+            if (inLow == "help")
+            {
+                Console.WriteLine("Help:");
+            }
+            else if (inLow == "about")
+            {
+                Console.WriteLine("Admiral OS - Alpha 0.1");
+                Console.WriteLine("Copyright 2022 BritishGeekGuy");
+                Console.WriteLine("This Operating System's Soruce Code is licensed under GPL-3.0");
+            }
+            else
+            {
+                Console.WriteLine("Bad Command!");
+            }
         }
     }
 }
